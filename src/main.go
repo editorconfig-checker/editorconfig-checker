07e5350ca7b961d4842fd0a39cd03470d9ab35c3
Max Strübing <mxstrbng@gmail.com>
2018-10-03 21:30:18 +0200

    feat(files): walk recursivly over dirs and only check unique files

--- src/main.go
+++ src/main.go
@@ -15,11 +15,12 @@ const version string = "0.0.1"
 type Params struct {
 	version bool
 	help    bool
-	// source directories and/or files which should be validated
-	sources []string
+	// directories and/or files which should be validated
+	rawFiles []string
 }
 
 // Global variable to store the cli parameter
+// only the init function should write to this variable
 var params Params
 
 // Init function, runs on start automagically
@@ -34,15 +35,72 @@ func init() {
 	// parse flags
 	flag.Parse()
 
-	// get remaining args as source directories
-	var rawSources = flag.Args()
-	if len(rawSources) == 0 {
-		// set sources to . (current working directory) if no parameters are passed
-		rawSources = []string{"."}
+	// get remaining args as rawFiles
+	var rawFiles = flag.Args()
+	if len(rawFiles) == 0 {
+		// set rawFiles to . (current working directory) if no parameters are passed
+		rawFiles = []string{"."}
 	}
-	// loop over sources to make them absolute
+
+	params.rawFiles = rawFiles
+}
+
+// Returns wether a path is a directory or not
+func isDirectory(path string) bool {
+	fi, _ := os.Stat(path)
+	return fi.Mode().IsDir()
+}
+
+// Checks wether a path of a file or directory exists or not
+func pathExists(path string) (bool, error) {
+	_, err := os.Stat(path)
+	if err == nil {
+		return true, nil
+	}
+	if os.IsNotExist(err) {
+		return false, nil
+	}
+	return true, err
+}
+
+// Converts a path to an absolute path, if already absolute it returns the original one
+func makePathAbsolute(path string) (string, error) {
+	if filepath.IsAbs(path) {
+		return path, nil
+	}
+
+	absolutePath, err := filepath.Abs(path)
+	return absolutePath, err
+}
+
+// Returns wether a slice contains a specific element
+func contains(slice []string, element string) bool {
+	for _, sliceElement := range slice {
+		if element == sliceElement {
+			return true
+		}
+	}
+	return false
+}
+
+// Adds a file to a slice if it isn't already in there
+// and returns the new slice
+func addToFiles(files []string, file string) []string {
+	if !contains(files, file) {
+		return append(files, file)
+	}
+
+	return files
+}
+
+// Returns all files which should be checked
+// @TODO: filtering/gitignore
+func getFiles() []string {
+	var files []string
+
+	// loop over rawFiles to make them absolute
 	// and check if they exist
-	for _, value := range rawSources {
+	for _, value := range params.rawFiles {
 		absolutePath, err := makePathAbsolute(value)
 		if err != nil {
 			panic(err)
@@ -57,28 +115,27 @@ func init() {
 			panic(err)
 		}
 
-		params.sources = append(params.sources, absolutePath)
-	}
-}
+		// if the path is an directory walk through it and add all files to files slice
+		if isDirectory(absolutePath) {
+			err := filepath.Walk(absolutePath, func(path string, f os.FileInfo, err error) error {
+				if !isDirectory(path) {
+					files = addToFiles(files, path)
+				}
+				return nil
+			})
 
-func pathExists(path string) (bool, error) {
-	_, err := os.Stat(path)
-	if err == nil {
-		return true, nil
-	}
-	if os.IsNotExist(err) {
-		return false, nil
-	}
-	return true, err
-}
+			if err != nil {
+				panic(err)
+			}
 
-func makePathAbsolute(path string) (string, error) {
-	if filepath.IsAbs(path) {
-		return path, nil
+			continue
+		}
+
+		// just add the absolutePath to files
+		files = addToFiles(files, absolutePath)
 	}
 
-	absolutePath, err := filepath.Abs(path)
-	return absolutePath, err
+	return files
 }
 
 func main() {
@@ -93,8 +150,11 @@ func main() {
 		return
 	}
 
-	// do real stuff
-	fmt.Println(params.sources)
+	// contains all files which should be checked
+	files := getFiles()
+
+	// do real stuff aka validation
+	fmt.Println(files)
 
 	fmt.Println("Run Forrest, run!!!")
 }
