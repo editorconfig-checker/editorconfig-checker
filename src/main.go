9a9af10d1fda47aa172f30eb70db49927b3b122c
Max Strübing <mxstrbng@gmail.com>
2018-10-08 07:02:19 +0200

    add verbose flag and some validating boilerplate

--- src/main.go
+++ src/main.go
@@ -7,6 +7,7 @@ import (
 	"os"
 	"os/exec"
 	"path/filepath"
+	"strconv"
 	"strings"
 )
 
@@ -17,8 +18,21 @@ const version string = "0.0.1"
 type Params struct {
 	version bool
 	help    bool
+	verbose bool
 	// directories and/or files which should be validated
 	rawFiles []string
+}
+
+// Represents one validation error
+type ValidationError struct {
+	line        int
+	description string
+}
+
+// Represents which errors occured in a file
+type ValidationErrors struct {
+	file   string
+	errors []ValidationError
 }
 
 // Global variable to store the cli parameter
@@ -28,11 +42,13 @@ var params Params
 // Init function, runs on start automagically
 func init() {
 	// define flags
-	flag.BoolVar(&params.version, "version", false, "print the number")
+	flag.BoolVar(&params.version, "version", false, "print the version number")
 	flag.BoolVar(&params.version, "v", false, "print the version number")
 
 	flag.BoolVar(&params.help, "help", false, "print the help")
 	flag.BoolVar(&params.help, "h", false, "print the help")
+
+	flag.BoolVar(&params.verbose, "verbose", false, "print debugging information")
 
 	// parse flags
 	flag.Parse()
@@ -65,16 +81,6 @@ func pathExists(path string) (bool, error) {
 	return true, err
 }
 
-// Converts a path to an absolute path, if already absolute it returns the original one
-func makePathAbsolute(path string) (string, error) {
-	if filepath.IsAbs(path) {
-		return path, nil
-	}
-
-	absolutePath, err := filepath.Abs(path)
-	return absolutePath, err
-}
-
 // Returns wether a slice contains a specific element
 func contains(slice []string, element string) bool {
 	for _, sliceElement := range slice {
@@ -87,8 +93,8 @@ func contains(slice []string, element string) bool {
 
 // Checks if the file is ignored by the gitignore
 // TODO: Remove dependency to git?
+// TODO: In this state the application has to be run out of the repository root or some sub-folder
 func isIgnoredByGitignore(file string) bool {
-	isInGitFolder(file)
 	cmd := exec.Command("git", "check-ignore", file)
 	err := cmd.Run()
 	if err != nil {
@@ -106,7 +112,7 @@ func isInGitFolder(file string) bool {
 // Adds a file to a slice if it isn't already in there
 // and returns the new slice
 func addToFiles(files []string, file string) []string {
-	if !contains(files, file) && !isIgnoredByGitignore(file) && !isInGitFolder(file) {
+	if !contains(files, file) && !isInGitFolder(file) && !isIgnoredByGitignore(file) {
 		return append(files, file)
 	}
 
@@ -121,7 +127,7 @@ func getFiles() []string {
 	// loop over rawFiles to make them absolute
 	// and check if they exist
 	for _, value := range params.rawFiles {
-		absolutePath, err := makePathAbsolute(value)
+		absolutePath, err := filepath.Abs(value)
 		if err != nil {
 			panic(err)
 		}
@@ -137,8 +143,10 @@ func getFiles() []string {
 
 		// if the path is an directory walk through it and add all files to files slice
 		if isDirectory(absolutePath) {
+			// TODO: Performance optimization - this is the bottleneck and loops over every folder/file
+			// and then checks if should be added. This needs some refactoring.
 			err := filepath.Walk(absolutePath, func(path string, f os.FileInfo, err error) error {
-				if !isDirectory(path) {
+				if !f.IsDir() {
 					files = addToFiles(files, path)
 				}
 				return nil
@@ -158,6 +166,30 @@ func getFiles() []string {
 	return files
 }
 
+// Validates a single file and returns the errors
+func validateFile(file string) []ValidationError {
+	var errors []ValidationError
+
+	// TODO: actual validation
+	for i := 0; i < 10; i++ {
+		validationError := ValidationError{i, strconv.Itoa(i)}
+		errors = append(errors, validationError)
+	}
+
+	return errors
+}
+
+// Validates all files and returns an array of validation errors
+func validateFiles(files []string) []ValidationErrors {
+	var validationErrors []ValidationErrors
+
+	for _, file := range files {
+		validationErrors = append(validationErrors, ValidationErrors{file, validateFile(file)})
+	}
+
+	return validationErrors
+}
+
 // Main function, dude
 func main() {
 	// Check for returnworthy params
@@ -166,16 +198,16 @@ func main() {
 		fmt.Println(version)
 		return
 	case params.help:
-		fmt.Println("Should print help!!!")
+		fmt.Println("USAGE:")
 		flag.PrintDefaults()
 		return
 	}
 
 	// contains all files which should be checked
 	files := getFiles()
+	errors := validateFiles(files)
 
-	// do real stuff aka validation
-	fmt.Println(files)
+	fmt.Println(errors)
 	fmt.Printf("%d files found!\n", len(files))
 
 	fmt.Println("Run Forrest, run!!!")
