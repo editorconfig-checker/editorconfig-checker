1ea1f9ed961d9efc17cd5df5dddcf066d7c6458c
Max Strübing <mxstrbng@gmail.com>
2019-03-01 12:30:41 +0100

    chore: tag release 1.1.1

--- cmd/editorconfig-checker/main.go
+++ cmd/editorconfig-checker/main.go
@@ -22,7 +22,7 @@ import (
 )
 
 // version
-const version string = "1.1.0"
+const version string = "1.1.1"
 
 // Global variable to store the cli parameter
 // only the init function should write to this variable
