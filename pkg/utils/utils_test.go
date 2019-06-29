42d3d4901de6c4e75bcdf17ccc5c7927d5f03a38
Max Strübing <mxstrbng@gmail.com>
2019-06-29 21:37:50 +0200

    chore: rename jepg to jpeg in test

--- pkg/utils/utils_test.go
+++ pkg/utils/utils_test.go
@@ -100,7 +100,7 @@ func TestGetRelativePath(t *testing.T) {
 }
 
 func TestDefaultExcludes(t *testing.T) {
-	result := "yarn\\.lock$|package-lock\\.json|composer\\.lock$|\\.snap$|\\.otf$|\\.woff$|\\.woff2$|\\.eot$|\\.ttf$|\\.gif$|\\.png$|\\.jpg$|\\.jepg$|\\.mp4$|\\.wmv$|\\.svg$|\\.ico$|\\.bak$|\\.bin$|\\.pdf$|\\.zip$|\\.gz$|\\.tar$|\\.7z$|\\.bz2$|\\.log$|\\.css\\.map$|\\.js\\.map$|min\\.css$|min\\.js$"
+	result := "yarn\\.lock$|package-lock\\.json|composer\\.lock$|\\.snap$|\\.otf$|\\.woff$|\\.woff2$|\\.eot$|\\.ttf$|\\.gif$|\\.png$|\\.jpg$|\\.jpeg$|\\.mp4$|\\.wmv$|\\.svg$|\\.ico$|\\.bak$|\\.bin$|\\.pdf$|\\.zip$|\\.gz$|\\.tar$|\\.7z$|\\.bz2$|\\.log$|\\.css\\.map$|\\.js\\.map$|min\\.css$|min\\.js$"
 
 	if DefaultExcludes != result {
 		t.Error("Expected default excludes to match", result)
