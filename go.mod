bcf7bc7eaae0ef0de0adfa0e287281e54beda76f
Max Strübing <mxstrbng@gmail.com>
2019-08-07 12:22:26 +0200

    fix(logger): don't print colors on windows
    closes #74

--- go.mod
+++ go.mod
@@ -2,4 +2,7 @@ module github.com/editorconfig-checker/editorconfig-checker
 
 go 1.12
 
-require github.com/editorconfig/editorconfig-core-go/v2 v2.0.0
+require (
+	github.com/editorconfig/editorconfig-core-go/v2 v2.0.0
+	golang.org/x/sys v0.0.0-20190215142949-d0b11bdaac8a
+)
---
+++ pkg/logger/logger_windows.go
@@ -0,0 +1,34 @@
+package logger
+
+import (
+	"fmt"
+	"os"
+)
+
+// Colors which can be used
+const (
+	YELLOW = "\x1b[33;1m"
+	GREEN  = "\x1b[32;1m"
+	RED    = "\x1b[31;1m"
+	RESET  = "\x1b[33;0m"
+)
+
+// Warning prints a warning message to Stdout in yellow
+func Warning(message string) {
+	Print(message, YELLOW, os.Stdout)
+}
+
+// Error prints an error message to Stderr in red
+func Error(message string) {
+	Print(message, RED, os.Stderr)
+}
+
+// Output prints a message on Stdout in 'normal' color
+func Output(message string) {
+	Print(message, RESET, os.Stdout)
+}
+
+// Print prints a message to a given stream in a defined color
+func Print(message string, color string, stream *os.File) {
+	fmt.Fprintf(stream, "%s\n", message)
+}
