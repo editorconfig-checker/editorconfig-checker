4f8fca6b2fbb9048b8e51b564b075b130402fa03
Max Strübing <max.struebing@farbenmeer.de>
2021-02-02 09:27:50 +0100

    chore(release): 2.3.2

--- pkg/config/config_test.go
+++ pkg/config/config_test.go
@@ -80,7 +80,7 @@ func TestMerge(t *testing.T) {
 
 	mergeConfig = Config{
 		ShowVersion:         true,
-		Version:             "2.3.1",
+		Version:             "2.3.2",
 		Help:                true,
 		DryRun:              true,
 		Path:                "some-other",
