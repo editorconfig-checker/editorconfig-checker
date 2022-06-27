73fe9421761287a32134b2018f805a1758e10e5e
Ross Smith II <ross@smithii.com>
2022-06-27 08:49:15 -0700

    Fix URL in download-test-files.sh

--- pkg/encoding/testdata/download-test-files.sh
+++ pkg/encoding/testdata/download-test-files.sh
@@ -7,18 +7,18 @@ git clone --depth 1 https://github.com/golang/text/
 git clone --depth 1 https://github.com/stain/encoding-test-files
 git clone --depth 1 https://github.com/baulk/chardet
 
-wget -O utf8-sdl.txt https://github.com/libsdl-org/SDL/blob/HEAD/test/utf8.txt
+wget -O utf8-sdl.txt https://raw.githubusercontent.com/libsdl-org/SDL/HEAD/test/utf8.txt
 
-cp --force --backup=simple --suffix=-mimetype.html mimetype/testdata/*.html .
+cp -v --force --backup=simple --suffix=-mimetype.html mimetype/testdata/*.html .
 
-cp --force --backup=simple --suffix=-mimetype.txt mimetype/testdata/*.txt .
+cp -v --force --backup=simple --suffix=-mimetype.txt mimetype/testdata/*.txt .
 
-cp --force --backup=simple --suffix=-text.txt text/encoding/testdata/*.txt .
+cp -v --force --backup=simple --suffix=-text.txt text/encoding/testdata/*.txt .
 
-cp --force --backup=simple --suffix=-encoding-test-files.txt encoding-test-files/*.txt .
+cp -v --force --backup=simple --suffix=-encoding-test-files.txt encoding-test-files/*.txt .
 
-cp --force --backup=simple --suffix=-chardet.html chardet/testdata/*.html .
+cp -v --force --backup=simple --suffix=-chardet.html chardet/testdata/*.html .
 
-rm -f json.float.txt json.int.txt json.string.txt not.srt.2.txt not.srt.txt
+rm -f json*.txt not*.txt
 
 sha256sum *.html *.txt | grep -v sha256sums\.txt >sha256sums.txt
