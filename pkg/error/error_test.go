ae80e7ce03528018a8775fb6e1ed7fd19ebd83b3
Andre Klärner <kandre@ak-online.be>
2024-08-16 22:06:02 +0200

    test: design a test case for error.ConsolidateErrors()

--- pkg/error/error_test.go
+++ pkg/error/error_test.go
@@ -2,6 +2,7 @@ package error
 
 import (
 	"errors"
+	"reflect"
 	"testing"
 
 	"github.com/editorconfig-checker/editorconfig-checker/v3/pkg/config"
@@ -109,6 +110,37 @@ func TestGetErrorCount(t *testing.T) {
 	count = GetErrorCount(input)
 	if count != 2 {
 		t.Error("Expected three error slice to have exactly one erorr errors, got", count)
+	}
+}
+
+func TestConsolidateErrors(t *testing.T) {
+	input := []ValidationError{
+		// two messages that become one block
+		{LineNumber: 1, Message: errors.New("message kind one")},
+		{LineNumber: 2, Message: errors.New("message kind one")},
+		// one message with a good line between it and the last bad line, but repeating the message
+		{LineNumber: 4, Message: errors.New("message kind one")},
+		// one message that breaks the continousnes of line 4 to line 6
+		{LineNumber: 5, Message: errors.New("message kind two")},
+		{LineNumber: 6, Message: errors.New("message kind one")},
+		// one message without a line number, that will become sorted to the top
+		{LineNumber: -1, Message: errors.New("file-level error")},
+	}
+
+	expected := []ValidationError{
+		{LineNumber: -1, Message: errors.New("file-level error")},
+		{LineNumber: 1, ConsecuitiveCount: 1, Message: errors.New("message kind one")},
+		{LineNumber: 4, Message: errors.New("message kind one")},
+		{LineNumber: 5, Message: errors.New("message kind two")},
+		{LineNumber: 6, Message: errors.New("message kind one")},
+	}
+
+	actual := ConsolidateErrors(input, config.Config{})
+
+	if !reflect.DeepEqual(expected, actual) {
+		t.Log("consolidation expectation          :", expected)
+		t.Log("consolidation actual returned value:", actual)
+		t.Error("returned list of validation errors deviated from the expected set")
 	}
 }
 
