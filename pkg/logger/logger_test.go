c960b41e66dd8e2e16e183b54ed6334972fb655b
Andre Klärner <kandre@ak-online.be>
2024-09-15 20:04:58 +0200

    fix(test): these tests should actually verify that their behaviour is correct

--- pkg/logger/logger_test.go
+++ pkg/logger/logger_test.go
@@ -1,51 +1,86 @@
 package logger
 
 import (
+	"bytes"
 	"testing"
+
+	"github.com/gkampitakis/go-snaps/snaps"
 )
 
-// Wannbe tests
+// interface for the closures passed to snapHelper
+type loggerTest func()
+
+// a helper function, that runs a closure with a logger, but redirects the Logger to write into a buffer, and then does snapshot testing on the buffer content
+func snapHelper(t *testing.T, logger *Logger, test loggerTest) {
+	t.Helper()
+	buffer := bytes.Buffer{}
+	logger.SetWriter(&buffer)
+	test()
+	snaps.MatchSnapshot(t, buffer.String())
+}
 
 func TestLoggerDebug(t *testing.T) {
 	logger := GetLogger()
 
-	logger.Debug("hello")
+	snapHelper(t, &logger, func() {
+		logger.Debug("hello")
+	})
 
-	logger.Debugg = true
-	logger.Debug("hello")
+	snapHelper(t, &logger, func() {
+		logger.Debugg = true
+		logger.Debug("hello")
+	})
 }
 
 func TestLoggerVerbose(t *testing.T) {
 	logger := GetLogger()
 
-	logger.Verbose("hello")
+	snapHelper(t, &logger, func() {
+		logger.Verbose("hello")
+	})
 
-	logger.Verbosee = true
-	logger.Verbose("hello")
+	snapHelper(t, &logger, func() {
+		logger.Verbosee = true
+		logger.Verbose("hello")
+	})
 }
 
 func TestLoggerWarning(t *testing.T) {
 	logger := GetLogger()
-	logger.Warning("bla%s", "hey")
 
-	logger.NoColor = true
-	logger.Warning("bla%s", "hey")
+	snapHelper(t, &logger, func() {
+		logger.Warning("bla%s", "hey")
+	})
+
+	snapHelper(t, &logger, func() {
+		logger.NoColor = true
+		logger.Warning("bla%s", "hey")
+	})
 }
 
 func TestLoggerOutput(t *testing.T) {
 	logger := GetLogger()
-	logger.Output("bla%s", "hey")
+
+	snapHelper(t, &logger, func() {
+		logger.Output("bla%s", "hey")
+	})
 }
 
 func TestLoggerError(t *testing.T) {
 	logger := GetLogger()
-	logger.Error("bla%s", "hey")
+	snapHelper(t, &logger, func() {
+		logger.Error("bla%s", "hey")
+	})
 
-	logger.NoColor = true
-	logger.Error("bla%s", "hey")
+	snapHelper(t, &logger, func() {
+		logger.NoColor = true
+		logger.Error("bla%s", "hey")
+	})
 }
 
 func TestPrintColor(t *testing.T) {
 	logger := GetLogger()
-	logger.printColor("Hello", RED)
+	snapHelper(t, &logger, func() {
+		logger.printColor("Hello", RED)
+	})
 }
