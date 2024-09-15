f812f76fba3cb00042357fa2910e6dfb6bbcf402
Andre Klärner <kandre@ak-online.be>
2024-09-15 16:02:05 +0200

    refactor: turn all static Logger methods into instance methods

--- pkg/logger/logger.go
+++ pkg/logger/logger.go
@@ -28,6 +28,12 @@ type Logger struct {
 	writer   io.Writer
 }
 
+func GetLogger() Logger {
+	logger := Logger{}
+	logger.Init()
+	return logger
+}
+
 // initialize the Logger to write to standard output
 func (l *Logger) Init() {
 	l.writer = os.Stdout
@@ -42,7 +48,7 @@ func (l *Logger) SetWriter(w io.Writer) {
 func (l Logger) Debug(format string, a ...interface{}) {
 	if l.Debugg {
 		message := fmt.Sprintf(format, a...)
-		Println(message)
+		l.Println(message)
 	}
 }
 
@@ -50,7 +56,7 @@ func (l Logger) Debug(format string, a ...interface{}) {
 func (l Logger) Verbose(format string, a ...interface{}) {
 	if l.Verbosee {
 		message := fmt.Sprintf(format, a...)
-		Println(message)
+		l.Println(message)
 	}
 }
 
@@ -58,9 +64,9 @@ func (l Logger) Verbose(format string, a ...interface{}) {
 func (l Logger) Warning(format string, a ...interface{}) {
 	message := fmt.Sprintf(format, a...)
 	if l.NoColor {
-		Println(message)
+		l.Println(message)
 	} else {
-		PrintlnColor(message, YELLOW)
+		l.PrintlnColor(message, YELLOW)
 	}
 }
 
@@ -85,56 +91,33 @@ func (l Logger) PrintLogMessages(messages []LogMessage) {
 	}
 }
 
-// Warning prints a warning message to Stdout in yellow
-func Warning(format string, a ...interface{}) {
-	message := fmt.Sprintf(format, a...)
-	PrintlnColor(message, YELLOW)
-}
-
 // Output prints a message on Stdout in 'normal' color
 func (l Logger) Output(format string, a ...interface{}) {
 	message := fmt.Sprintf(format, a...)
-	Println(message)
-}
-
-// Output prints an error message to Stdout in 'normal' color
-func Output(format string, a ...interface{}) {
-	message := fmt.Sprintf(format, a...)
-	Println(message)
+	l.Println(message)
 }
 
 // Error prints an error message to Stdout in red
 func (l Logger) Error(format string, a ...interface{}) {
 	message := fmt.Sprintf(format, a...)
 	if l.NoColor {
-		Println(message)
+		l.Println(message)
 	} else {
-		PrintlnColor(message, RED)
+		l.PrintlnColor(message, RED)
 	}
 }
 
-// Error prints an error message to Stdout in red
-func Error(format string, a ...interface{}) {
-	message := fmt.Sprintf(format, a...)
-	PrintlnColor(message, RED)
-}
-
-// Print prints a message
-func Print(message string) {
-	fmt.Printf("%s", message)
-}
-
 // Println prints a message with a trailing newline
-func Println(message string) {
-	fmt.Printf("%s\n", message)
+func (l Logger) Println(message string) {
+	fmt.Fprintf(l.writer, "%s\n", message)
 }
 
 // PrintColor prints a message in a given ANSI-color
-func PrintColor(message string, color string) {
-	fmt.Printf("%s%s%s", color, message, RESET)
+func (l Logger) PrintColor(message string, color string) {
+	fmt.Fprintf(l.writer, "%s%s%s", color, message, RESET)
 }
 
 // PrintlnColor prints a message in a given ANSI-color with a trailing newline
-func PrintlnColor(message string, color string) {
-	fmt.Printf("%s%s%s\n", color, message, RESET)
+func (l Logger) PrintlnColor(message string, color string) {
+	fmt.Fprintf(l.writer, "%s%s%s\n", color, message, RESET)
 }
