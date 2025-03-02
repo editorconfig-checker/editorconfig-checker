75ddca48d8fb0f6800c40f8fddcc34307128b313
Andre Klärner <kandre@ak-online.be>
2025-03-02 17:01:17 +0100

    build: go1.24 vet validates first arg of printf now
    see https://go.dev/doc/go1.24#vet

--- pkg/files/files.go
+++ pkg/files/files.go
@@ -61,7 +61,7 @@ func AddToFiles(filePaths []string, filePath string, config config.Config) []str
 
 	if err != nil {
 		config.Logger.Error("Could not get the ContentType of file: %s", filePath)
-		config.Logger.Error(err.Error())
+		config.Logger.Error("%v", err.Error())
 	}
 
 	isExcluded, err := IsExcluded(filePath, config)
