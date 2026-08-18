216e7479e443ff8a744e89e562603a00031d07c3
Théo LUDWIG <contact@theoludwig.fr>
2026-08-18 14:09:01 +0800

    chore: disable commitlint footer-max-line-length rule (#605)

--- commitlint.config.js
+++ commitlint.config.js
@@ -2,5 +2,6 @@ module.exports = {
     extends: ["@commitlint/config-conventional"],
     rules: {
         "body-max-line-length": [0, "never", []],
+        "footer-max-line-length": [0, "never", []],
     },
 };
